5177df165fe1ce4a58f18ba24bb4bd535de04879
Ariana Cabana <acabana.0904@gmail.com>
2017-06-27 15:00:42 -0500

    sin conexion al api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(_ =>{
-  $.getJSON('https://laboratoria.cuadra.co:9339/api/v1/students/',(data)=>{
+  $.getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/',(data)=>{
     console.log(data);
     $.each(data,(i,obj)=>{
 			$("#list").append('<li>'+obj.name+'<input type="checkbox"></li>');

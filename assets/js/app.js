ee3b111bcb06bc2261c04b8e1fc99079ad3e0715
Ariana Cabana <acabana.0904@gmail.com>
2017-06-27 13:42:15 -0500

    obteniendo lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,8 @@
-const ListName = (data) =>{
-  const element = $('<li>'+data[]+'</li>');
-  const checkb = $('<input type="checkbox">');
-
-  $('#list').append(element);
-  element.append(checkb);
-}
-
 $(_ =>{
   $.getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/',(data)=>{
-    console.log(data[0].name);
-    ListName(data);
+    console.log(data);
+    $.each(data,(i,obj)=>{
+			$("ol").append('<li>'+obj.name+'<input type="checkbox"></li>');
+		});
   });
 });

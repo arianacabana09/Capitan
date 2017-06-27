1f340982a4803029902341d30b34a32f2ad08864
Ariana Cabana <acabana.0904@gmail.com>
2017-06-27 13:48:21 -0500

    cambiando de posicion

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ $(_ =>{
   $.getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/',(data)=>{
     console.log(data);
     $.each(data,(i,obj)=>{
-			$("ol").append('<li>'+obj.name+'<input type="checkbox"></li>');
+			$("#list").append('<li>'+obj.name+'<input type="checkbox"></li>');
 		});
   });
 });
